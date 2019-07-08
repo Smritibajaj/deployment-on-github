88be8aabe8a1554ee3f759efc2bc748898a3a208
Simmy Bajaj <simmy.bajaj@allocatesoftware.com>
2019-07-08 17:14:27 +0530

    Updates

---
+++ precache-manifest.ebf30a115f2e407262a8f07755691d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3f72cf8a57e879871cae59fbdde003",
+    "url": "/deployment-on-github/index.html"
+  },
+  {
+    "revision": "42eaaa0afe42e9ad8ccf",
+    "url": "/deployment-on-github/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/deployment-on-github/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "42eaaa0afe42e9ad8ccf",
+    "url": "/deployment-on-github/static/js/main.252dcd60.chunk.js"
+  },
+  {
+    "revision": "522872c2962c466a2c8f",
+    "url": "/deployment-on-github/static/js/runtime~main.89ce618d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployment-on-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
